372368a0f29ba38b026afdfcffe395d7a052958c
carlosilias <carlosilias98@gmail.com>
2022-02-14 15:36:46 -0500

    1 binaries uploaded

--- index.js
+++ index.js
@@ -6,29 +6,102 @@ const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const opt = {
-  method: "GET",
+  method: 'GET',
 };
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto", opt)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    appDiv.innerHTML = `<center><h1>Pokemon</center>
+fetch('https://pokeapi.co/api/v2/pokemon/ditto', opt)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    appDiv.innerHTML = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand mb-0 h1" href="#">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="" width="100" height="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Acerca Ditto</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://fondosmil.com/fondo/12145.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+  <p>© 2022 Carlos Ilias, Universidad de la Costa - Desarrollo Web 2</p>
+  </div>
+</footer>
+
+
+
+    <!--<center><h1>Pokemon</center>
     <table border=2>
       
       <tr>
         <th>Nombre</th>
         <th>Fuerza</th>
-        <th>Poder</th>
-        <th>XP</th>
+        <th>Alto</th>
+        <th>Ancho</th>
       </tr>
       <tr>
+       <td>${data.name}</td>
        <td>${data.id}</td>
-       <td>${data.name}</td>
-       <td>${data.name}</td>
-       <td>${data.name}</td>
+       <td>${data.height}</td>
+       <td>${data.weight}</td>
       </tr>
-    </table>
+    </table>-->
     `;
-    
   });
